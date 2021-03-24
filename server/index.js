58a5deddb3a95a8fe3760e200b42edbf4a368078
Arya Krishnan <aryakrishnan@berkeley.edu>
2021-03-24 14:37:17 -0700

    checkpoint 2

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express'); 
 const socketio = require('socket.io'); 
 const http = require('http'); 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users,js');
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,16 +12,25 @@ const server = http.createServer(app);
 const io = socketio(server); 
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection!');
-
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name, room);
-
-        const error = true;
-        if(error) {
-            callback({ error: 'error' });
-        }
+        const { error, user } = addUser({ id: socket.id, name, room });
         
+        if(error) return callback(error);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}!`});
+        socket.broadcast.to(user.room).emit({ user: 'admin', text : `Welcome ${user.name} to our game!`});
+
+        socket.join(user.room);
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id); 
+        
+        io.to(user.room).emit('message', {user: user.name, text: message});
+
+        callback();
     });
 
     socket.on('disconnect', () => {
